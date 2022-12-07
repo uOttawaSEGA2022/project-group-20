fe35d58cc1cdfd4b8c02629b904c272faf005e89
Jemma <90709315+KelyMac@users.noreply.github.com>
2022-12-07 09:14:25 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/mealer/ClientComplaint.java
@@ -0,0 +1,14 @@
+package com.example.mealer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClientComplaint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_complaint);
+    }
+}
