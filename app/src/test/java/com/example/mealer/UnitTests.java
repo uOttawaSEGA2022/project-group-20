85a9a4784d8f5dee2093eb960016a468bfe99deb
layano3 <lomar018@uottawa.ca>
2022-12-07 22:50:42 -0500

    added 8 unit tests

--- app/src/test/java/com/example/mealer/UnitTests.java
+++ app/src/test/java/com/example/mealer/UnitTests.java
@@ -37,4 +37,72 @@ public class UnitTests {
         ClientInfo client = new ClientInfo("emma","emma@gmail.com","emma123");
         assertTrue(client.getFirstnameField() == "emma");
     }
+
+    @Test
+    public void checkGetEmailAddressField(){
+        ClientInfo client = new ClientInfo("emma","emma@gmail.com","emma123");
+        assertTrue(client.getEmailAddressField().equals("emma@gmail.com"));
+    }
+
+    @Test
+    public void checkGetPasswordField(){
+        ClientInfo client = new ClientInfo("emma","emma@gmail.com","emma123");
+        assertTrue(client.getPasswordField().equals("emma123"));
+    }
+
+    @Test
+    public void checkAddCart(){
+        MainActivity activity = new MainActivity();
+        DataBaseHelper DB = new DataBaseHelper(activity);
+
+        DB.addCart("Pizza");
+        assertTrue(DB.getCartItems().getCount() > 0);
+    }
+
+    @Test
+    public void checkAddMeal(){
+        MainActivity activity = new MainActivity();
+        DataBaseHelper DB = new DataBaseHelper(activity);
+
+        DB.addMeal("Pizza");
+        assertTrue(DB.getMeals().getCount()>0);
+    }
+
+    @Test
+    public void checkComplaints(){
+        MainActivity activity = new MainActivity();
+        DataBaseHelper DB = new DataBaseHelper(activity);
+
+        DB.removeAllComplaints();
+        assertTrue(DB.getComplaints().getCount() == 0);
+
+    }
+
+    @Test
+    public void checkAddComplaint(){
+        MainActivity activity = new MainActivity();
+        DataBaseHelper DB = new DataBaseHelper(activity);
+
+        DB.addComplaints("Bad", "jim");
+        assertTrue(DB.getComplaints().getCount() >0);
+    }
+
+    @Test
+    public void checkSuspension(){
+        MainActivity activity = new MainActivity();
+        DataBaseHelper DB = new DataBaseHelper(activity);
+
+        DB.suspendAccount("jim","10");
+        assertTrue(DB.isAccountSuspended("jim"));
+    }
+
+    @Test
+    public void checkSuspensionDuration(){
+        MainActivity activity = new MainActivity();
+        DataBaseHelper DB = new DataBaseHelper(activity);
+
+        DB.suspendAccount("jim","10");
+        assertTrue(DB.getSuspendedAccountDuration("jim") == "10");
+    }
+
 }
