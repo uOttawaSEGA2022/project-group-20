486527119553810c1a5bf7872debf8455a59a0c1
Sihan Sun <113392876+ChrisSihan@users.noreply.github.com>
2022-12-10 00:02:20 -0500

    Update HandleRequestActivity.java

--- app/src/main/java/com/example/mealer/HandleRequestActivity.java
+++ app/src/main/java/com/example/mealer/HandleRequestActivity.java
@@ -1,8 +1,14 @@
 package com.example.mealer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import java.util.ArrayList;
@@ -32,7 +38,29 @@ public class HandleRequestActivity extends AppCompatActivity {
             cart.add(cp);
             c.moveToNext();
         }
+        
 
+
+
+    }
+    public void notification(String s){
+
+        NotificationCompat.Builder brick = new NotificationCompat.Builder(this,"notify");
+        brick.setContentTitle("Your meal request has changed its status");
+        brick.setContentText("Your purchase has been: " + s);
+        brick.setSmallIcon(R.drawable.ic_launcher_foreground);
+        brick.setAutoCancel(true);
+
+        NotificationManagerCompat manager =  NotificationManagerCompat.from(this);
+        manager.notify(1,brick.build());
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            NotificationChannel channel = new NotificationChannel("notify", "NotifChannel", NotificationManager.IMPORTANCE_HIGH);
+            NotificationManager nmanager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+
+        }
 
 
     }
@@ -41,6 +69,7 @@ public class HandleRequestActivity extends AppCompatActivity {
         String meal = text.getText().toString();
         if(cart.contains(meal)){
             DB.setStatus(meal, "approved");
+            notification("approved");
         }
         else{
             Toast.makeText(this, "You don't have a purchase request for this meal",Toast.LENGTH_LONG).show();
@@ -53,10 +82,13 @@ public class HandleRequestActivity extends AppCompatActivity {
         text = (EditText) findViewById(R.id.mn2);
         String meal = text.getText().toString();
         if(cart.contains(meal)){
-            DB.setStatus(meal, "approved");
+            DB.setStatus(meal, "rejected");
+            notification("rejected");
         }
         else{
             Toast.makeText(this, "You don't have a purchase request for this meal",Toast.LENGTH_LONG).show();
         }
     }
+
+
 }
