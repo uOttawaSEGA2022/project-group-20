7a9bae7235e027bf05363ac86bc1e8fdc055b729
Sihan Sun <113392876+ChrisSihan@users.noreply.github.com>
2022-12-09 23:08:17 -0500

    Update HandleRequestActivity.java

--- app/src/main/java/com/example/mealer/HandleRequestActivity.java
+++ app/src/main/java/com/example/mealer/HandleRequestActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
-
 import java.util.ArrayList;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class HandleRequestActivity extends AppCompatActivity {
     DataBaseHelper DB;
@@ -47,13 +49,14 @@ public class HandleRequestActivity extends AppCompatActivity {
 
 
     }
-    public void rejectButton(View view)       { 
-    text = (EditText) findViewById(R.id.mn2);
-    String meal = text.getText().toString();
+    public void rejectButton(View view)       {
+        text = (EditText) findViewById(R.id.mn2);
+        String meal = text.getText().toString();
         if(cart.contains(meal)){
-        DB.setStatus(meal, "approved");
+            DB.setStatus(meal, "approved");
         }
         else{
-        Toast.makeText(this, "You don't have a purchase request for this meal",Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "You don't have a purchase request for this meal",Toast.LENGTH_LONG).show();
         }
     }
+}
